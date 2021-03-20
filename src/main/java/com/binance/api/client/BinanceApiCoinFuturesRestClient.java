95fa34f93e037fe3310bf0f28756d475800f8527
Cavassoni <lkvassoni@hotmail.com>
2021-03-20 20:29:24 -0300

    repository

--- src/main/java/com/binance/api/client/BinanceApiCoinFuturesRestClient.java
+++
@@ -1,8 +0,0 @@
-package com.binance.api.client;
-
-import com.binance.api.client.domain.account.FuturesCoinAccount;
-
-public interface BinanceApiCoinFuturesRestClient {
-
-    FuturesCoinAccount getFuturesCoinAccount() ;
-}
--- src/main/java/com/binance/api/client/BinanceApiUsdFuturesRestClient.java
+++
@@ -1,9 +0,0 @@
-package com.binance.api.client;
-
-import com.binance.api.client.domain.account.FuturesUsdAccount;
-
-public interface BinanceApiUsdFuturesRestClient {
-
-	FuturesUsdAccount getFuturesUsdAccount();
-
-}
